a70e1e66902cc34170c55a86b1a5e67cec3da94f
Evelein <34264640+Evelein@users.noreply.github.com>
2018-07-08 12:21:26 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		API
+	</title>
+		<link href="style.css" rel="stylesheet">
+</head>
+<body>
+	<div class="layer1">
+		<form method="post" action="input1.php">
+    		<input name="inputSearch" type="text" size="40" placeholder="Введи информацию для поиска">
+    			<button type="submit">
+    				Поиск
+    			</button>
+  		</form>
+	</div>
+	<div class="layer2">
+		<form method="post" action="input2.php">
+			<button type="submit">
+    			Категории
+    		</button>
+    	</form>
+	</div>
+</body>
+</html>
