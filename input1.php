763cad45971876928a89a3d8cf53904484c28129
Evelein <34264640+Evelein@users.noreply.github.com>
2018-07-08 12:21:56 +0300

    Create input1.php

---
+++ input1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="style.css" rel="stylesheet">
+		<title>
+			API
+		</title>
+	</head>
+	<body>
+		<div align="left">
+			<table border="2">
+    			<thead>
+    				<tr>
+				        <th height="25">Название</th>
+				        <th>Страна</th>
+				        <th>Производитель</th>
+				        <th>Категория</th>
+        			</tr>
+    			</thead>
+    				<?php
+					$test = $_POST['inputSearch']; //POST-запрос 
+					require 'phpmongodb/find.php';
+					?>
+			</table>
+		</div>
+	</body>
+</html>
