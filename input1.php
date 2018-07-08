0a709e1832d8ad86e822846bfff67e56ffe24580
Evelein <34264640+Evelein@users.noreply.github.com>
2018-07-08 13:51:42 +0300

    Update input1.php

--- input1.php
+++ input1.php
@@ -9,18 +9,18 @@
 	<body>
 		<div align="left">
 			<table border="2">
-    			<thead>
-    				<tr>
+    				<thead>
+    					<tr>
 				        <th height="25">Название</th>
 				        <th>Страна</th>
 				        <th>Производитель</th>
 				        <th>Категория</th>
-        			</tr>
-    			</thead>
+        				</tr>
+    				</thead>
     				<?php
 					$test = $_POST['inputSearch']; //POST-запрос 
 					require 'phpmongodb/find.php';
-					?>
+				?>
 			</table>
 		</div>
 	</body>
